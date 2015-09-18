19c5d1535f00541a5737985dd1a680b8c352c74a
Leslie Nguyen <leslieknguyen@gmail.com>
2015-09-18 17:09:30 +0000

    More debugging

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -22,11 +22,11 @@ $(document).ready(function() {
   }); // form submission
   function stripeResponseHandler(status, response) {
     // Get a reference to the form:
-    var f = $("#new_user");
+    var f = $('.new_user');
     // Get the token from the response:
     var token = response.id;
     // Add the token to the form:
-    f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+    f.append('<input type="hidden" name="user[stripe_card_token]" value="'+ token + '" />');
     // Submit the form:
     f.get(0).submit(); 
   }
